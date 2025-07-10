0b7ff432564e82ad70fa32208b86875f2ae619f8
Nowaaru <nowaaru@proton.me>
2025-07-10 13:40:59 -0700

    fix: noire

--- noire/default.nix
+++ noire/default.nix
@@ -2,59 +2,58 @@
 # remove all of these and start
 # using Folder/Default.nix
 {
-  config,
+  programs,
   configure,
   user,
   ...
 }: {
   imports = [
     # Developer goodies.
-    # programs.dev.formatters.dprint
-    #
-    # programs.dev.langs.javascript.typescript
-    # programs.dev.langs.javascript.node
-    # programs.dev.langs.javascript.bun
-    #
-    # programs.dev.langs.dotnet
-    # programs.dev.langs.rust
-    # programs.dev.langs.lua
-    #
-    # programs.dev.nvim
-    #
-    # programs.dev.git
-    # programs.dev.lazygit
+    programs.dev.formatters.dprint
+
+    programs.dev.langs.javascript.typescript
+    programs.dev.langs.javascript.node
+    programs.dev.langs.javascript.bun
+
+    programs.dev.langs.dotnet
+    programs.dev.langs.lua
+
+    programs.dev.nvim
+
+    programs.dev.git
+    programs.dev.lazygit
 
     # Gaming and the like.
-    # programs.gaming.minecraft
-    # programs.gaming.lutris
-    # programs.gaming.steam
-    # programs.gaming.osu
+    programs.gaming.minecraft
+    programs.gaming.lutris
+    programs.gaming.steam
+    programs.gaming.osu
 
     # Social media and social utilities.
-    # programs.social.discord.vesktop
+    programs.social.discord.vesktop
 
     # General things, auxiliary functionalities.
-    # programs.misc.qol.night-light
-    # programs.misc.qol.kdenlive
-    # programs.misc.qol.obsidian
-    # programs.misc.bitwarden
-    #
-    # programs.misc.terminal.all
-    #
-    # programs.misc.dunst
-    # programs.misc.print-screen
-    #
-    # programs.misc.video
-    # programs.misc.audio
-    # programs.misc.clip
-    # programs.misc.disk
-    # programs.misc.dir
-    # programs.misc.obs
-    #
+    programs.misc.qol.night-light
+    programs.misc.qol.kdenlive
+    programs.misc.qol.obsidian
+    programs.misc.bitwarden
+
+    programs.misc.terminal.all
+
+    programs.misc.dunst
+    programs.misc.print-screen
+
+    programs.misc.video
+    programs.misc.audio
+    programs.misc.clip
+    programs.misc.disk
+    programs.misc.dir
+    programs.misc.obs
+
     # Skate through the world, there's no one way to grind...
-    # programs.entertainment.music.mixers.ncpamixer
-    # programs.entertainment.music.players.spotify
-    # programs.entertainment.jellyfin
+    programs.entertainment.music.mixers.ncpamixer
+    programs.entertainment.music.players.spotify
+    programs.entertainment.jellyfin
 
     # Games by those who rule the world.
     # user.programs.an-anime-team.anime-game
